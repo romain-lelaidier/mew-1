16536c62aca7053ddef89d98993aa6bb999cab82
romain-lelaidier <romain.lelaidier@etu.minesparis.psl.eu>
2025-10-28 01:00:26 +0100

    Player main block navigation

--- web/player/utils.jsx
+++ web/player/utils.jsx
@@ -54,25 +54,23 @@ export function PInfos(props) {
   return (
     <>
       <A onClick={() => player.start(player.s.current.id)} href={url(player.s.current)} class="font-bold">{player.s.current.name}</A>
-      <Show when={player.s.current.albums}>
-        <div class="flex flex-row items-center">
-          <For each={player.s.current.albums}>{(album, i) => 
+      <Show when={player.s.current.artists}>
+        <div class="flex flex-row">
+          <For each={player.s.current.artists}>{(artist, i) => 
             <>
-              <Show when={i() == 0}><span style="display: inline-block; margin-bottom: -0.5em"><Icon type="record-vinyl" size={1}/></span></Show>
               <Show when={i() > 0}><span class="mr-1">,</span></Show>
-              <A onClick={() => player.start(album.id)} href={`/player/${album.id}`} class="italic">{album.name}</A>
+              <A href={`/artist/${artist.id}`}>{artist.name}</A>
             </>
           }
           </For>
         </div>
       </Show>
-      <Show when={player.s.current.artists}>
+      <Show when={player.s.current.albums}>
         <div class="flex flex-row">
-          <For each={player.s.current.artists}>{(artist, i) => 
+          <For each={player.s.current.albums}>{(album, i) => 
             <>
-              <Show when={i() == 0}><span style="display: inline-block; margin-bottom: -0.5em"><Icon type="user" size={1}/></span></Show>
               <Show when={i() > 0}><span class="mr-1">,</span></Show>
-              <A href={`/artist/${artist.id}`}>{artist.name}</A>
+              <A onClick={() => player.start(album.id)} href={`/player/${album.id}`} class="italic">{album.name}</A>
             </>
           }
           </For>
